16ec77a6b8ddbcaeed804246b888c594df55b486
Yahya Hafez <yhafez3@gmail.com>
2022-11-25 14:46:58 -0800

    feat: add axe testing

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,8 +1,18 @@
+// Use axe to test the accessibility of the app
 import { render } from '@testing-library/react'
+import { axe, toHaveNoViolations } from 'jest-axe'
 import Home from '../pages'
+
+expect.extend(toHaveNoViolations)
 
 describe('App', () => {
 	it('should work as expected', () => {
 		render(<Home />)
+	})
+
+	it('should be accessible', async () => {
+		const { container } = render(<Home />)
+		const results = await axe(container)
+		expect(results).toHaveNoViolations()
 	})
 })
